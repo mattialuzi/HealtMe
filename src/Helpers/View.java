1a035bbf53e8caca903f770e74f107aeae1450f2
Paolo <grossetti.paolo@libero.it>
2017-01-02 16:41:31 +0100

    prova

---
+++ src/Helpers/View.java
@@ -0,0 +1,16 @@
+package Helpers;
+
+import javax.swing.*;
+/**
+ * Created by ALLDE on 02/01/2017.
+ */
+public class View {
+
+    private JPanel panel;
+
+    public View()
+    {
+        panel= new JPanel();
+    }
+
+}
