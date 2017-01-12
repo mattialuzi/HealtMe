5131c28a28d13d39802407493e7c73818f47072b
lorenzobraconi <lorenzobraconi1994@gmail.com>
2017-01-12 12:28:33 +0100

    imbusta

---
+++ src/View/Alimentazione/GiornoAlimView.java
@@ -0,0 +1,17 @@
+package View.Alimentazione;
+
+import javax.swing.*;
+
+/**
+ * Created by lorenzobraconi on 12/01/17.
+ */
+public class GiornoAlimView {
+    private JPanel mainPanel;
+
+    public GiornoAlimView() {
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+}
--- src/View/Alimentazione/IndexAlimentazioneView.java
+++ src/View/Alimentazione/IndexAlimentazioneView.java
@@ -7,12 +7,30 @@ import javax.swing.*;
  */
 public class IndexAlimentazioneView {
     private JPanel mainPanel;
+    private JTabbedPane tabbedPane1;
+    private JPanel lunediPanel;
+    private JPanel martediPanel;
+    private JPanel mercolediPanel;
+    private JPanel giovediPanel;
+    private JPanel venerdiPanel;
+    private JPanel sabatoPanel;
+    private JPanel domenicaPanel;
 
     public IndexAlimentazioneView() {
     }
 
     public JPanel getMainPanel() {
         return mainPanel;
+    }
+
+    private void createUIComponents() {
+        lunediPanel =  new GiornoAlimView().getMainPanel();
+        martediPanel = new GiornoAlimView().getMainPanel();
+        mercolediPanel = new GiornoAlimView().getMainPanel();
+        giovediPanel = new GiornoAlimView().getMainPanel();
+        venerdiPanel = new GiornoAlimView().getMainPanel();
+        sabatoPanel = new GiornoAlimView().getMainPanel();
+        domenicaPanel = new GiornoAlimView().getMainPanel();
     }
 }
 
