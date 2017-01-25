763ea03f60536f5f9739c83dca1de7634c4d62b6
lorenzobraconi <lorenzobraconi1994@gmail.com>
2017-01-25 18:35:16 +0100

    mbust

--- src/View/Alimentazione/ProgAlimManView.java
+++ src/View/Alimentazione/ProgAlimManView.java
@@ -1,10 +1,12 @@
 package View.Alimentazione;
 
 import javax.swing.*;
+
 /**
  * Created by lorenzobraconi on 25/01/17.
  */
 public class ProgAlimManView {
+
     private JPanel mainPanel;
 
     public ProgAlimManView() {
@@ -13,6 +15,4 @@ public class ProgAlimManView {
     public JPanel getMainPanel() {
         return mainPanel;
     }
-
-
 }
