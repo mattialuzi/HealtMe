95bc256e5ce4b655ab1e3e216153dbf325e3ca2d
Wall <mattialuzi@gmail.com>
2017-02-05 22:22:28 +0100

    provato grafico

---
+++ src/View/Graph.java
@@ -0,0 +1,57 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Graph extends JPanel {
+    private ArrayList<Integer> punti;
+    // disyanza dal bordo del pannello in px  (per tutte direzioni)
+    private int border;
+    //massimo valore ordinata
+    private int yMax;
+
+    public Graph (ArrayList<Integer> punti){
+        this.punti = punti;
+        this.border = 30;
+        this.yMax =11;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        //setta la larghezza in pixel di una unita del grafico sia per l'asciss ache per l'ordinata
+        int xScale = (getWidth() - 2 * border) / (punti.size() - 1);
+        int yScale = (getHeight() - 2 * border) / (yMax - 1);
+        //disegna asse x e y, (considera che le coordinate hanno riferieìmento)
+        // 0 ----->
+        // |
+        // |
+        // V
+        g2.drawLine(border, getHeight() - border, border, border);
+        g2.drawLine(border, getHeight() - border, getWidth() - border, getHeight() - border);
+        //disegno quadrettatatura
+
+        for(int i=1; i< punti.size(); i++) {
+            int x0 = border + i * xScale ;
+            int y0 = border;
+            int x1=x0;
+            int y1= getHeight() - border;
+            g2.drawLine(x0,y0,x1,y1);
+        }
+        g2.setFont(g2.getFont().deriveFont(yScale));
+        for (int i=1; i< yMax; i++) {
+            int y0 = getHeight() - border - i * yScale;
+            int x0 = border;
+            int y1=y0;
+            int x1= getWidth() - border ;
+            g2.drawLine(x0,y0,x1,y1);
+            g2.drawString(String.valueOf(i),border/4,y0+yScale/2);
+        }
+
+
+    }
+
+}
--- src/View/Riepilogo/ProgressiView.java
+++ src/View/Riepilogo/ProgressiView.java
@@ -1,17 +1,29 @@
 package View.Riepilogo;
 
+import View.Graph;
+
 import javax.swing.*;
+import java.util.ArrayList;
 
 /**
  * Created by lorenzobraconi on 04/02/17.
  */
 public class ProgressiView {
     private JPanel mainPanel;
+    private JPanel graphPanel;
 
     public ProgressiView() {
     }
 
     public JPanel getMainPanel() {
         return mainPanel;
+    }
+
+    private void createUIComponents() {
+        ArrayList<Integer> test = new ArrayList<>();
+        test.add(0);
+        test.add(1);
+        test.add(2);
+        graphPanel = new Graph(test);
     }
 }
